5a072bb30dd05a17f861efb85d577f23eb747a2e
Aditya Pillai <pillaiaditya2310@gmail.com>
2026-03-03 15:32:44 +0530

    feat: Setup done.

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AppRoutes from "./router/AppRoutes";
+
+// import Navbar from "./components/layout/Navbar";
+// import Footer from "./components/layout/Footer";
+
+// function App() {
+//   return (
+//     <Router>
+//       {/* Global Layout Wrapper */}
+//       <div className="min-h-screen flex flex-col bg-background text-foreground">
+        
+//         {/* Navigation */}
+//         <Navbar />
+
+//         {/* Main Content */}
+//         <main className="flex-grow">
+//           <AppRoutes />
+//         </main>
+
+//         {/* Footer */}
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Tailwind Working
+    </div>
+  )
+}
